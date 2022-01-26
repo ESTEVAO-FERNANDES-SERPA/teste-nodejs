116cec6701e7aab231644fb514609f3e75b32464
ESTEVAO <estevao.serpa1234@gmail.com>
2022-01-26 14:12:44 -0300

    Criando posts via json

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+
+
 const app  = express()
 
 // definindo o template engine
@@ -21,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+//habilita server para receber dados via post(formulario)
+app.use(express.urlencoded({extended:true}))
+
+
+
 //MVC -MODEL VIEW CONTROLLER
 
 //rotas
@@ -29,21 +37,49 @@ app.get('/', (req, res) => {
         title: 'Digital Tech - Home'
     })
 })
+app.get('/cadastros', (req, res) => { 
+    const{c} = req.query
+    res.render('cadastros',{
+        title: 'Digital Tech - Cadastro de Posts',
+        cadastrado: c,
+    })
+})
+app.post('/salvar', (req, res) => { 
+    const{titulo, texto} = req.body
+
+    const data = fs.readFileSync('./store/posts.json')
+    const posts = JSON.parse(data)
+
+    posts.push({
+        titulo,
+        texto,
+    })
+
+
+    const postsString = JSON.stringify(posts)
+
+    fs.writeFileSync('./store/posts.json', postsString)
+
+    res.redirect('/cadastros?c=1')
+})
 app.get('/posts',(req, res) => { 
     res.render('posts',{
         title:'Digital Tech - Posts',
         posts:[
             {
                 title:'Novidades no mundo da tecnologia',
-                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.'
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.',
+                star: 3,
             },
             {
                 title:'Criando um servidor com nodejs',
-                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.'
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.',
+                
             },
             {
                 title:'JS é legal',
-                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.'
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eos suscipit optio, necessitatibus deleniti sint libero voluptate illo enim, officia debitis, eveniet accusamus porro? Voluptatum dolorum quae impedit fugiat.',
+                star: 5,
             }
         ]
     })
